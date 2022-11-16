6f1704c982d2ee942b497138b2d06e4526e1f4ad
aditya-bhat646 <adityabhat646@gmail.com>
2022-11-16 10:49:16 +0530

    Stateful Logic Introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [addedUsers, setAddedUsers] = useState([]);
+
+  const addedUsersHandler = (person) => {
+    setAddedUsers((prevList) => ([...prevList, person]));
+  };
+
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <AddUser onAddUsers={addedUsersHandler} />
+      <UsersList users={addedUsers} />
+    </>
   );
 }
 
